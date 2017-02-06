55f99d48f0138c5219f58efd2c0bc2e51ada5b7f
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-06 15:42:03 -0600

    firebase init, route provider

--- app/app.js
+++ app/app.js
@@ -1,2 +1,21 @@
-const app = angular.module('juxtaPosition', ['ngRoute'])
-  .config()
+const app = angular
+  .module('juxtaPosition', ['ngRoute'])
+  .config(($routeProvider) => {
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyC3aWQn02Qm3-XRKmPKfCk4pgVVZ51zyLM",
+      authDomain: "juxta-position.firebaseapp.com",
+      databaseURL: "https://juxta-position.firebaseio.com",
+      storageBucket: "juxta-position.appspot.com",
+      messagingSenderId: "967364970214"
+    });
+
+    $routeProvider
+      .when('/', {
+        controller: "mainCtrl",
+        templateUrl: "/app/partials/main.html",
+        // resolve:
+
+      })
+
+  })
