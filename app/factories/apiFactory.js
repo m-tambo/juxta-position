9b22e710b2b56f10f4cfb57dfa09995409807a23
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-07 00:02:22 -0600

    added an api call

--- app/factories/apiFactory.js
+++ app/factories/apiFactory.js
@@ -1,4 +1,17 @@
 app
-  .factory('apiFactory', function() {
+  .factory('apiFactory', function($http) {
 
+    let getPlayers = function () {
+      console.log('ready for api call')
+      return $http
+        .get('http://www.fantasyfootballnerd.com/service/players/json/iz33m4ducg4h​/')
+        .then((res) => {
+          console.log(res)
+        })
+
+    }
+
+    return {
+      getPlayers
+    }
   })
