1badee59bafc07d5015b735ff3c2f531b2631f32
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-06 22:49:08 -0600

    added postComp function

--- app/factories/firebaseFactory.js
+++ app/factories/firebaseFactory.js
@@ -1,4 +1,15 @@
 app
   .factory('firebaseFactory', function() {
+    return {
+      postComp : () => {
+        let newComp = {};
+
+        return $http
+          .post('https://freakin-music-history.firebaseio.com/.json', newComp)
+          .then(() => {
+            return newComp
+          })
+      }
+    }
 
   })
