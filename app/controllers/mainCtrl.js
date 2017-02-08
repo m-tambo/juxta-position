a0e9fc1f8f7e87b5b1fedbf2d72577ce87bbb90f
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-08 12:55:50 -0600

    added player cards

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -4,10 +4,25 @@ app
     $scope.playerList = players;
     console.log(players)
 
-    $scope.juxtaPose = function() {
-      // console.log($scope.inputx, $scope.inputy)
+  // ____ auto-complete function ____
+    $(function () {
+        var single = $('.input-1').materialize_autocomplete({
+            multiple: {
+                enable: false
+            },
+            dropdown: {
+                el: '#singleDropdown',
+                itemTemplate: '<li class="ac-item" data-id="<%= item.id %>" data-text=\'<%= item.text %>\'><a href="javascript:void(0)"><%= item.highlight %></a></li>'
+            },
+            onSelect: function (item) {
+                console.log(item.text + ' was selected');
+            }
+        });
+    });
 
-      // find player X
+    $scope.juxtaPose = function() {
+
+    // ____ find player X ____
       for (let i = 0; i < $scope.playerList.length; i++) {
         if ($scope.inputx === ($scope.playerList[i].player.FirstName + " " + $scope.playerList[i].player.LastName)) {
           $scope.playerX = $scope.playerList[i]
@@ -15,7 +30,7 @@ app
         }
       }
 
-      // find player Y
+    //____ find player Y ____
       for (let i = 0; i < $scope.playerList.length; i++) {
         if ($scope.inputy === ($scope.playerList[i].player.FirstName + " " + $scope.playerList[i].player.LastName)) {
           $scope.playerY = $scope.playerList[i]
@@ -27,12 +42,12 @@ app
 
     }
 
-    $scope.showPlayers() {
-      if (playerX === undefined || playerY === undefined) {
+    $scope.showPlayers = function () {
+      if ($scope.playerX === undefined || $scope.playerY === undefined) {
         alert("To see comparison, enter two valid Player names")
       }
       else {
-
+        document.querySelector('.player-versus').removeAttribute('hidden')
       }
     }
 
