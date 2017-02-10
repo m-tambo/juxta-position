0cf14a4ce6cb896120f6d2435f2986cd0f74d77f
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-10 11:16:48 -0600

    added authFactory

--- app/factories/authFactory.js
+++ app/factories/authFactory.js
@@ -1,6 +1,23 @@
 app
-  .factory('authFactory', function() {
+  .factory('authFactory', function($q) {
     return {
+      login (email, pass) {
+        // converts native ES6 promise to angular promise so no $scope.$apply needed
+        return $q.resolve(firebase.auth().signInWithEmailAndPassword(email, pass))
+      },
 
+      getUser () {
+        return $q((resolve, reject) => {
+          // http://stackoverflow.com/questions/37370224/firebase-stop-listening-onauthstatechanged
+          const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+            unsubscribe()
+            if (user) {
+              resolve(user)
+            } else {
+              reject()
+            }
+          })
+        })
+      }
     }
   })
