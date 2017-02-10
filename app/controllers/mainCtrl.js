e1b345a5e72fec7a15dcee48ca1bc68a3953d2fa
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-10 10:18:17 -0600

    displaying comparison table

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,6 +1,7 @@
 app
   .controller('mainCtrl', function(players, $scope, apiFactory) {
 
+    $scope.weeks = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16"]
     $scope.playerList = players;
     console.log(players)
 
@@ -24,11 +25,16 @@ app
       $scope.setPlayers('inputx', 'playerX');
       $scope.setPlayers('inputy', 'playerY');
 
-      $scope.showPlayers();
 
       $scope.showProjections($scope.playerX.player, 'projectionsX');  // find playerX projections, set obj to var
       $scope.showProjections($scope.playerY.player, 'projectionsY');  // find playerY projections, set obj to var
 
+      $scope.showRankings($scope.playerX.player, 'rankingsX', 'positionXrank')
+      $scope.showRankings($scope.playerY.player, 'rankingsY', 'positionYrank')
+
+      // $scope.showStats()
+
+      $scope.showData();
 
     }
 
@@ -38,18 +44,19 @@ app
       for (let i = 0; i < $scope.playerList.length; i++) {
         if ($scope[input] === ($scope.playerList[i].player.FirstName + " " + $scope.playerList[i].player.LastName)) {
           $scope[output] = $scope.playerList[i]
-          console.log('player:', $scope[output]['player'])
+          // console.log('player:', $scope[output]['player'])
         }
       }
     }
 
 
-    $scope.showPlayers = function () {
+    $scope.showData = function () {
       if ($scope.playerX === undefined || $scope.playerY === undefined) {
         alert("To see comparison, enter two valid Player names")
       }
       else {
         document.querySelector('.player-versus').removeAttribute('hidden')
+        document.querySelector('.table').removeAttribute('hidden')
       }
     }
 
@@ -66,8 +73,26 @@ app
         })
     }
 
-    $scope.showRankings = function () {
-      apiFactory.getNerdRankings()
+    $scope.showRankings = function (dude, letter, pos) {
+      apiFactory.getNerdRankings() // (dude, [week#])
+        .then((rankings) => {
+          for (j = 0; j < rankings.length; j++) {
+            if (rankings[j].name === (dude.FirstName + " " + dude.LastName)) {
+              $scope[letter] = rankings[j]
+              $scope[pos] = j + 1
+              console.log('position rank:', $scope[pos], 'rankings: ', $scope[letter])
+            }
+          }
+        })
+    }
+
+    $scope.showStats = function () {
+      apiFactory.getNflStats()
+        .then((stats) => {
+          // for (k = 0; k < stats.length; k++) {
+            console.log(stats)
+
+        })
     }
 
   })
