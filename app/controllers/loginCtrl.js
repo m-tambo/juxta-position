58ad4e56df242b6f227d79b20abe9a8c38d4366d
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-10 12:22:58 -0600

    login page ready for auth

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -4,11 +4,11 @@ app
     $scope.goToRegister = function () {
       document.querySelector('.login').setAttribute('hidden', 'hidden')
       document.querySelector('.register').removeAttribute('hidden')
-    }
+    };
 
     $scope.loginUser = function () {
       console.log('clicked')
-    }
+    };
 
     $scope.registerUser = function () {
       console.log('clicked')
