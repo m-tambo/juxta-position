75620a460e8b089fe516945c944c7757246fedf1
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-14 14:35:23 -0600

    select element in progress

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,13 +1,7 @@
 app
   .controller('mainCtrl', function(players, $scope, apiFactory, firebaseFactory) {
     console.log('mainCtrl firing')
-    $scope.weeks = [{value: "Week 1"}, {value: "Week 2"}, {value: "Week 3"}, {value: "Week 4"}, {value: "Week 5"}, {value: "Week 6"}, {value: "Week 7"}, {value: "Week 8"}, {value: "Week 9"}, {value: "Week 10"}, {value: "Week 11"}, {value: "Week 12"}, {value: "Week 13"}, {value: "Week 14"}, {value: "Week 15"}, {value: "Week 16"}, ]
-    // $scope.weeks = {
-    //   model: null,
-    //   availableOptions: [
-    //     {'id': '1', 'name': 'Week 1'}, {'id': '2', 'name': 'Week 2'}, {'id': '3', 'name': 'Week 3'}, {'id': '4', 'name': 'Week 4'}, {'id': '5', 'name': 'Week 5'}, {'id': '6', 'name': 'Week 6'}, {'id': '7', 'name': 'Week 7'}, {'id': '8', 'name': 'Week 8'}, {'id': '9', 'name': 'Week 9'}, {'id': '10', 'name': 'Week 10'}, {'id': '11', 'name': 'Week 11'}, {'id': '12', 'name': 'Week 12'}, {'id': '13', 'name': 'Week 13'}, {'id': '14', 'name': 'Week 14'}, {'id': '15', 'name': 'Week 15'}, {'id': '16', 'name': 'Week 16'}
-    //   ]
-    // };
+    $scope.weeks = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16"]
 
     $scope.playerList = players;
 
@@ -130,16 +124,7 @@ app
     }
 
     // _____ materialize stuff _____
-    $('#drops').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    });
+    $('select').material_select();
 
     $('.carousel.carousel-slider').carousel({fullWidth: true});
 
