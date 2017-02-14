5c0deb1675f513bed450ca42f4c0444cbe45c740
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-14 15:11:27 -0600

    profile pic src on $scope

--- app/controllers/profileCtrl.js
+++ app/controllers/profileCtrl.js
@@ -3,5 +3,5 @@ app
     console.log('profile controller firing')
 
     $scope.user = firebase.auth().currentUser
-
+    $scope.profilePic = 'http://nexus1492.eu/wp-content/plugins/smartcat_our_team/inc/img/noprofile.jpg'
   })
