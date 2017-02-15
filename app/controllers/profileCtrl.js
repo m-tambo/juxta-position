2861037c5dbe7404e0bd1c0b3cbbffc3f0afcddf
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-15 10:13:05 -0600

    cleaned up navbar hiding and links

--- app/controllers/profileCtrl.js
+++ app/controllers/profileCtrl.js
@@ -5,6 +5,6 @@ app
     $scope.user = firebase.auth().currentUser
     $scope.profilePic = 'http://nexus1492.eu/wp-content/plugins/smartcat_our_team/inc/img/noprofile.jpg'
 
-    $scope.comps = [1, 2, 3, 4, 5]
-    // $scope.firebaseFactory.getComps;
+    $scope.comps = [1,2,3,4,5,6,7,8]
+    $scope.firebaseFactory.getComps;
   })
--- app/modules/directives.js
+++ app/modules/directives.js
@@ -11,16 +11,30 @@ dir
         $scope.user = firebase.auth().currentUser
         console.log($scope.user)
 
+        $scope.hideNav = function () {  // clear the nav bar when a link is selected
+          $('.button-collapse').sideNav('hide');
+        }
+
         $(".button-collapse").sideNav();
 
+        $scope.goToLogin = function () {
+          console.log('go to login')
+          document.querySelector('.login').removeAttribute('hidden');
+          document.querySelector('.register').setAttribute('hidden', 'hidden');
+          $scope.hideNav()
+        };
+
         $scope.goToRegister = function () {
-          document.querySelector('.login').setAttribute('hidden', 'hidden')
-          document.querySelector('.register').removeAttribute('hidden')
+          console.log('go to register')
+          document.querySelector('.login').setAttribute('hidden', 'hidden');
+          document.querySelector('.register').removeAttribute('hidden');
+          $scope.hideNav()
         };
 
         $scope.signOut = function () {
           authFactory.logout()
           $location.path('/')
+          $scope.hideNav()
         }
       },
       resolve: {
