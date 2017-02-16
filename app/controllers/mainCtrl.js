9437737b08fbcba26dc5a60bc967bebe0dc3f8f0
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-16 13:16:09 -0600

    hard-coded week options

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,7 +1,7 @@
 app
   .controller('mainCtrl', function(players, $scope, apiFactory, firebaseFactory, $location) {
     console.log('mainCtrl firing')
-    $scope.weeks = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16"]
+    // $scope.weeks = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16"]
     // $scope.uid = firebase.auth().currentUser.uid;
     $scope.playerList = players;
 
