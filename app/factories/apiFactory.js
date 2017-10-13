ed74f32652e106815b70054719bc2b642a5f2dba
Michael Tambornino <michael.tambornino@gmail.com>
2017-10-12 21:19:15 -0500

    Clean up api factory

--- app/factories/apiFactory.js
+++ app/factories/apiFactory.js
@@ -6,61 +6,49 @@ app
     const getPlayers = function () {
       return $http
         .get('/data/nflPlayerList.json')
-        .then( res => res.data.playerentry )
+        .then(res => res.data.players)
     };
 
     const getNerdProjections = function (pos, wk) {
       return $http
         .get(`${srvr}/nerdprojections/${pos}/${wk}`)
-        .then((res) => {
-          return res.data.Projections
-        })
+        .then(res => res.data.Projections)
     };
 
     const getNerdRankings = function (pos, wk) {
       return $http
         .get(`${srvr}/nerdrankings/${pos}/${wk}`)
-        .then((res) => {
-          return res.data.Rankings
-        })
+        .then(res => res.data.Rankings)
     };
 
     const getNflStats = function (wk) {
       return $http
         .get(`${srvr}/weekstats/${wk}`)
-        .then((res) => {
-          return res.data.players
-        })
+        .then(res => res.data.players)
       };
 
     const getNflLeaders = function () {
       return $http
         .get('http://api.fantasy.nfl.com/v1/players/scoringleaders?&count=30&season=2016&position=RB&format=json')
-        .then((res) => {
-          return res.data
-        })
+        .then(res => res.data)
     };
 
     const getSchedule = function () {
       return $http
         .get('/data/schedule.json')
-        .then((res) => {
-          return res.data.Schedule
-        })
+        .then(res => res.data.Schedule)
     };
 
     const getExpertRankings = function (wk, pos, exp) {
       return $http
         .get(`${srvr}/espnexperts/${wk}/${pos}/${exp}`)
-        .then((res) => {
-          return res.data.players
-        })
+        .then(res => res.data.players)
     };
 
     const getPlayerNews = function () {
       return $http
         .get(`${srvr}/playernews`)
-        .then( res => res.data.news )
+        .then(res => res.data.news)
     }
     return {
       getPlayers,
